b857a59d39e5b9e7acd30fac134368476f14eb25
mygetzu <mbenypangestu@gmail.com>
2019-05-08 23:00:03 +0700

    Add - pages collection, contact us

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,30 @@ import { NavHeaderComponent } from './component/nav-header/nav-header.component'
 import { FooterComponent } from './component/footer/footer.component';
 import { TestComponent } from './pages/test/test.component';
 
+import { NgBootstrapModule } from '@ng-bootstrap/ng-bootstrap';
+import { CollectionComponent } from './pages/collection/collection.component';
+import { ItemListComponent } from './pages/collection/item-list/item-list.component';
+import { ItemDetailComponent } from './pages/collection/item-detail/item-detail.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavHeaderComponent,
     FooterComponent,
-    TestComponent
+    TestComponent,
+    CollectionComponent,
+    ItemListComponent,
+    ItemDetailComponent,
+    ContactUsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgBootstrapModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
